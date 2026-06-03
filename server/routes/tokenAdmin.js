b9e26571efe38173870abd96c54723e4577f2a42
ashirov bahram <abuelectric96@gmail.com>
2026-06-04 01:25:40 +0500

    Fix authRequired import in tokenAdmin.js

--- server/routes/tokenAdmin.js
+++ server/routes/tokenAdmin.js
@@ -5,7 +5,7 @@
 
 const express = require('express');
 const router  = express.Router();
-const { authRequired } = require('../middleware/authRequired');
+const authRequired = require('../middleware/authRequired');
 const { refreshById }  = require('../lib/tokenRefresh');
 const db = require('../db');
 
